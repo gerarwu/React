d33dfc505fe89bd0edf8027cd6de63ff804e2afc
gmunoz <gmunoz@localhost.localdomain>
2018-07-17 16:25:35 -0500

    Navigation

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -3,11 +3,12 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => {    
     return (   
         <Aux>
-            <div>Toolbar, SideDrawer, Backdrop</div>
+            <Toolbar />
             <main className={classes.Content}>
                 {props.children}
             </main>
---
+++ burger/src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Logo.css';
+
+const logo = ()=> (
+    <div className={classes.Logo}>
+        <img src='https://picsum.photos/300/200?random' />
+    </div>
+);
+
+export default logo;
