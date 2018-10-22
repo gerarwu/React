4e6e0807083143491fabe4bfe6adff258e4b31f0
Gerar <gerar@localhost.localdomain>
2018-10-22 11:44:45 -0500

    Restructuring the folders

--- burger/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Bakcdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props)=>{
