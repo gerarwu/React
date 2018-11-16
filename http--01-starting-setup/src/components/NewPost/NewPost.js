0728bedaa95000f1bc0ac9a5055a1d88c55471be
Gerardo Muñoz García <gmunoz@igenter.com>
2018-11-16 17:03:34 -0600

    Requests with axios

--- http--01-starting-setup/src/components/NewPost/NewPost.js
+++ http--01-starting-setup/src/components/NewPost/NewPost.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
 class NewPost extends Component {
+    
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Gerardo'
+    }
+
+    addNewPostHandler = () => {
+        const data = { 
+            title: this.state.title,
+            content: this.state.content,
+            author: this.state.author
+        }
+        
+        axios.post("https://jsonplaceholder.typicode.com/posts/", data).then( repsonse => {
+            console.log(repsonse);
+        });
     }
 
     render () {
@@ -22,7 +36,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={ this.addNewPostHandler } >Add Post</button>
             </div>
         );
     }
--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,53 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        posts: [],
+        postSelected: null,
+        error: false
+    };
+
+    componentDidMount(){
+        
+        axios.get( "https://jsonplaceholder.typicode.com/postss" )
+        .then( response =>{
+            const posts = response.data.splice(0, 4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: "Gerardo M."
+                }
+            });            
+            this.setState({ posts: updatedPosts });
+        }).catch( error =>{
+            console.log(error);
+            this.setState({error: true});
+        });
+
+    }
+
+    postSelectedHandler = ( id ) => {        
+        this.setState({ postSelected: id });        
+    }
+
     render () {
+
+        let posts = <p class={{textAlign: 'center'}}> Somthing went wrong  =( !!!</p>;
+
+        if(!this.state.error){
+            posts = this.state.posts.map( post => { 
+                return <Post title={post.title} key={post.id} author={post.author} clicked={ () => this.postSelectedHandler(post.id) } />
+            });
+        }
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    { posts }
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.postSelected} />
                 </section>
                 <section>
                     <NewPost />
