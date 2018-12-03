ccbe79c14ebc4e1932dbd394b273e2136d959232
Gerardo Muñoz García <gmunoz@igenter.com>
2018-12-03 17:27:52 -0600

    Conditional redirects

--- http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
+++ http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -8,7 +9,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Gerardo'
+        author: 'Gerardo',
+        sended: false
     }
 
     addNewPostHandler = () => {
@@ -20,12 +22,20 @@ class NewPost extends Component {
         
         axios.post("/posts/", data).then( repsonse => {
             console.log(repsonse);
+            this.setState({sended: true});
         });
     }
 
     render () {
+
+        let redirect = null;
+        if(this.state.sended){
+            redirect = <Redirect to='/posts' />
+        }
+
         return (
             <div className="NewPost">
+                { redirect }
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
