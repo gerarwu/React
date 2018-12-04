869cab5840be33a56647609d6d21672181e3a3d1
Gerardo Muñoz García <gmunoz@igenter.com>
2018-12-04 10:03:25 -0600

    Loading routes lazy

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
+//import NewPost from './NewPost/NewPost';
+import AsyncComponent from '../../hoc/asyncComponent';
+
 import './Blog.css';
+
+const AsyncNewPost = AsyncComponent( ()=> {
+    return import ('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
@@ -21,7 +26,7 @@ class Blog extends Component {
 
                 
                 <Switch>
-                    <Route path='/new-post'  component={ NewPost } />
+                    <Route path='/new-post'  component={ AsyncNewPost } />
                     <Route path='/posts'  component={ Posts } />    
                     <Redirect from='/' to='/posts' />
                 </Switch>
