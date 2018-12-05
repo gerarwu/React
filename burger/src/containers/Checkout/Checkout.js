206035e7db85adacf4b165df1f7ab671463dfc8d
Gerardo Muñoz García <gmunoz@igenter.com>
2018-12-05 09:48:51 -0600

    Adding router

---
+++ burger/src/containers/Checkout/Checkout.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+class Checkout extends Component{
+    
+    state= {
+        ingredients : {
+            salad: 2,
+            meat: 4
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <CheckoutSummary ingredients={this.state.ingredients}/>
+            </div>
+        );
+    }
+}
+
+export default Checkout;
